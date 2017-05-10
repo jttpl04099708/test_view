b908b12062632642d7481c4e4fb16c8fe3035e5d
jiangtingting <1224576608@qq.com>
2017-05-10 23:43:14 +0800

    修改1.0

--- app/src/main/java/com/example/test_view/MainActivity.java
+++ app/src/main/java/com/example/test_view/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("sssss", "1.1.1.1");
-        Log.e("ssssss","1.2.2.2");
-        Log.e("ssssss","1.2");
+        Log.e("ssssss", "1.2.2.2");
+        Log.e("ssssss", "1.2");
+        Log.e("ssssss","1.2111");
     }
 }
